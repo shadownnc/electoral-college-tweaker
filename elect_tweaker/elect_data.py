ae81de6b6a3bfb989d71d9b0eae10674777f999e
Neil Chopra <contact@neilnchopra.com>
2016-11-13 11:10:48 -0800

    Moved data retrieval/conversion into elect_data.py

---
+++ elect_tweaker/elect_data.py
@@ -0,0 +1,128 @@
+from lxml import html
+import requests
+
+
+def get_html_from_webpage(url):
+    page = requests.get(url)
+    return html.fromstring(page.content)
+
+
+def get_html_from_file(path):
+    with open(path, 'r', encoding="utf8") as f:
+        return html.fromstring(f.read())
+
+
+def get_state_table_from_html(html):
+    tables = html.xpath('//table[@class="wikitable sortable"]')
+
+    state_table = None
+    for table in tables:
+        state_header = table.xpath('.//th[text()="State"]')
+        if state_header:
+            state_table = table
+            break
+
+    if state_table is None:
+        raise ImportError
+
+    return state_table
+
+
+def firstOrNone(result):
+    return result[0] if result and len(result) > 0 else None
+
+
+def build_candidate_index_from_state_table(state_table):
+    candidate_cells = state_table.xpath('.//th[@colspan=3]/text()')
+    candidate_index = []
+
+    for value in candidate_cells:
+        candidate_record = None
+        if len(candidate_index) > 0:
+            candidate_record = candidate_index[len(candidate_index) - 1]
+
+        if candidate_record is None or candidate_record["party"]:
+            candidate_index.append({
+                "name": value.strip(),
+                "party": None,
+                "winnerTakeAllVotes": 0,
+                "proportionedVotes": 0
+            })
+        elif candidate_record["party"] is None:
+            candidate_record["party"] = value.strip()
+
+    return candidate_index
+
+
+def build_state_data_from_state_row(state_row, candidate_index):
+    state_cells = state_row.getchildren()
+
+    # the state name will be in a link
+    state_name = firstOrNone(state_cells[0].xpath('.//a/text()'))
+
+    if not state_name:
+        return None
+
+    electoral_votes = state_cells[1].text
+
+    state_data = {
+        "name": state_name,
+        "electoralVotes": electoral_votes,
+        "topCandidate": None,
+        "candidateData": {}
+    }
+
+    top_candidate = None
+    top_percentage = 0
+
+    for i in range(0, len(candidate_index)):
+        candidate_name = candidate_index[i]["name"]
+
+        # each candidate spans 3 columns
+        vote_total_text = state_cells[3 * i + 2].text
+        vote_percentage_text = state_cells[3 * i + 3].text
+
+        vote_total = vote_total_text.replace("–", "0").replace(",", "")
+        percentage = float(vote_percentage_text.replace("–", "0").strip('%')) / 100
+        if percentage > top_percentage:
+            top_candidate = candidate_name
+            top_percentage = percentage
+
+        candidate_data = {
+            "popularVoteTotal": vote_total,
+            "popularVotePercentage": "{0:.4f}".format(percentage)
+        }
+        state_data["candidateData"][candidate_name] = candidate_data
+
+    state_data["topCandidate"] = top_candidate
+
+    return state_data
+
+
+def build_voting_data_from_state_table(state_table, candidate_index):
+    voting_data = {
+        "candidates": candidate_index,
+        "stateResults": []
+    }
+    state_rows = state_table.getchildren()
+
+    for row in state_rows:
+        state_data = build_state_data_from_state_row(row, candidate_index)
+
+        if state_data:
+            voting_data["stateResults"].append(state_data)
+
+    return voting_data
+
+
+def convert_wiki_to_json_data(path):
+    if path.startswith("http"):
+        html_tree = get_html_from_webpage(path)
+    else:
+        html_tree = get_html_from_file(path)
+
+    state_table = get_state_table_from_html(html_tree)
+    candidate_index = build_candidate_index_from_state_table(state_table)
+    voting_data = build_voting_data_from_state_table(state_table, candidate_index)
+
+    return voting_data
--- tweaker.py
+++ tweaker.py
@@ -1,133 +1,39 @@
-from lxml import html
-import requests
-import io
+from elect_tweaker import elect_data
 
-page = requests.get('https://en.wikipedia.org/wiki/United_States_presidential_election,_2008')
-tree = html.fromstring(page.content)
-
-#tree = None
-#with open('wikipedia_election_1996.html', 'r', encoding="utf8") as f:
-#    tree = html.fromstring(f.read())
-
-tables = tree.xpath('//table[@class="wikitable sortable"]')
-
-stateTable = None
-for table in tables:
-    stateHeader = table.xpath('.//th[text()="State"]')
-    if stateHeader:
-        stateTable = table
-        break
-
-if stateTable is None:
-    raise ImportError
-
-candidateArray = stateTable.xpath('.//th[@colspan=3]/text()')
-
-candidateIndex = []
-
-for value in candidateArray:
-    candidateRecord = None
-    if len(candidateIndex) > 0:
-        candidateRecord = candidateIndex[len(candidateIndex) - 1]
-
-    if candidateRecord is None or candidateRecord["party"]:
-        candidateIndex.append({
-            "name": value.strip(),
-            "party": None,
-            "winnerTakeAllVotes": 0,
-            "proportionedVotes": 0
-        })
-    elif candidateRecord["party"] is None:
-        candidateRecord["party"] = value.strip()
-
-#print(candidateIndex)
-
-stateRows = stateTable.getchildren()
-
-votingData = []
-
-#print(stateRows)
-
-def firstOrNone(result):
-    return result[0] if result and len(result) > 0 else None
-
-foundState = False
-for row in stateRows:
-    stateCells = row.getchildren()
-
-    #print(stateCells)
-
-    stateName = firstOrNone(stateCells[0].xpath('.//a/text()'))
-
-    if not foundState and stateName == 'Alabama':
-        foundState = True
-    elif not foundState or not stateName:
-        continue
-
-    electoralVotes = stateCells[1].text
-
-    stateData = {
-        "name": stateName,
-        "electoralVotes": electoralVotes,
-        "topCandidate": None,
-        "candidateData": {}
-    }
-
-    topCandidate = None
-    topPercentage = 0
-
-    for i in range(0, len(candidateIndex)):
-        candidateName = candidateIndex[i]["name"]
-
-        textVoteTotal = stateCells[3*i + 2].text
-        textPercentage = stateCells[3*i + 3].text
-
-        percentage = float(textPercentage.replace("–", "0").strip('%'))/100
-        if percentage > topPercentage:
-            topCandidate = candidateName
-            topPercentage = percentage
-
-        candidateData = {
-            "popularVoteTotal": textVoteTotal.replace("–", "0").replace(",", ""),
-            "popularVotePercentage": "{0:.4f}".format(percentage)
-        }
-        stateData["candidateData"][candidateName] = candidateData
-
-    stateData["topCandidate"] = topCandidate
-    votingData.append(stateData)
+voting_data = elect_data.convert_wiki_to_json_data('https://en.wikipedia.org/wiki/United_States_presidential_election,_1996')
 
 
-#print(votingData)
+def add_electoral_votes(candidate_name, candidate_index, algorithm, amount):
+    for candidate in candidate_index:
+        if candidate["name"] == candidate_name:
+            candidate[algorithm] = int(candidate[algorithm]) + int(amount)
 
-def addElectoralVotes(candidateName, key, amount):
-    for candidate in candidateIndex:
-        if candidate["name"] == candidateName:
-            candidate[key] = int(candidate[key]) + int(amount)
 
-def processWinnerTakeAll(votingData):
-    for stateData in votingData:
-        print("{}: Awarding all {} electoral votes to {}".format(stateData["name"], stateData["electoralVotes"], stateData["topCandidate"]))
-        addElectoralVotes(stateData["topCandidate"], "winnerTakeAllVotes", stateData["electoralVotes"])
+def process_winner_take_all(voting_data):
+    for state_data in voting_data["stateResults"]:
+        print("{}: Awarding all {} electoral votes to {}".format(state_data["name"], state_data["electoralVotes"], state_data["topCandidate"]))
+        add_electoral_votes(state_data["topCandidate"], voting_data["candidates"], "winnerTakeAllVotes", state_data["electoralVotes"])
 
-def processProportional(votingData):
-    for stateData in votingData:
-        totalElectoralVotes = float(stateData["electoralVotes"])
+
+def process_proportional(votingData):
+    for stateData in votingData["stateResults"]:
+        total_electoral_votes = float(stateData["electoralVotes"])
         allocated = 0
-        voteMap = {}
-        for candidateKey in stateData["candidateData"]:
-            candidateData = stateData["candidateData"][candidateKey]
-            proportionedVotes = int(totalElectoralVotes * float(candidateData["popularVotePercentage"]))
-            voteMap[candidateKey] = proportionedVotes
-            print("{}: Awarding {} electoral votes to {}".format(stateData["name"], voteMap[candidateKey], candidateKey))
-            addElectoralVotes(candidateKey, "proportionedVotes", proportionedVotes)
-            allocated = allocated + voteMap[candidateKey]
+        vote_map = {}
+        for candidate_key in stateData["candidateData"]:
+            candidate_data = stateData["candidateData"][candidate_key]
+            proportioned_votes = int(total_electoral_votes * float(candidate_data["popularVotePercentage"]))
+            vote_map[candidate_key] = proportioned_votes
+            if proportioned_votes > 0:
+                print("{}: Awarding {} electoral votes to {}".format(stateData["name"], proportioned_votes, candidate_key))
+            add_electoral_votes(candidate_key, voting_data["candidates"], "proportionedVotes", proportioned_votes)
+            allocated = allocated + vote_map[candidate_key]
 
         # any unallocated votes go to the top vote getter
-        addElectoralVotes(stateData["topCandidate"], "proportionedVotes", totalElectoralVotes - allocated)
+        add_electoral_votes(stateData["topCandidate"], voting_data["candidates"], "proportionedVotes", total_electoral_votes - allocated)
 
 
+process_winner_take_all(voting_data)
+process_proportional(voting_data)
 
-processWinnerTakeAll(votingData)
-processProportional(votingData)
-
-print(candidateIndex)
+print(voting_data["candidates"])
